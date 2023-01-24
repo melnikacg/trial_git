33ef48bed7f2d5e2690c0ff03476f8124edf2e1b
melnikoleg <melnikacg@gmail.com>
2023-01-24 20:10:16 +0300

    719 from mac

--- main.py
+++ main.py
@@ -6,7 +6,7 @@
 
 def print_hi(name):
     # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, hi , {name}')  # Press ⌘F8 to toggle the breakpoint.
+    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
     # small change from main
     #small change
 
